1d06834f6dcf2d28014475452f29729359f45e37
TJ Holowaychuk <tj@vision-media.ca>
2013-02-07 19:44:14 +0000

    add creation to example

--- examples/collections.js
+++ examples/collections.js
@@ -7,7 +7,11 @@ var client = new Cloudup({
   pass: 'Dev1'
 });
 
-client.collections(function(err, cols){
-  if (err) throw err;
-  console.log(cols);
+client
+.collection({ title: 'Animals' })
+.save(function(){
+  client.collections(function(err, cols){
+    if (err) throw err;
+    console.log(cols);
+  });
 });
