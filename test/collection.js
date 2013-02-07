2f01eb5973a344d18a0be199f13fc999f72c3cc0
TJ Holowaychuk <tj@vision-media.ca>
2013-02-07 18:32:08 +0000

    add .save() test

--- test/collection.js
+++ test/collection.js
@@ -1,16 +1,24 @@
 
 var Cloudup = require('..');
 
+var client = new Cloudup({
+  url: 'http://local.cloudup.com',
+  token: 'h1YXjGq3mru91ArPAAAD'
+});
+
 describe('Collection', function(){
   describe('.item(options)', function(){
     it('should create a new Item', function(){
-      var client = new Cloudup({
-        url: 'http://local.cloudup.com'
-      });
-
       var col = client.collection({ title: 'Something' });
       var item = col.item({ title: 'Some item' });
       item.constructor.name.should.equal('Item');
+    })
+  })
+
+  describe('.save(fn)', function(){
+    it('should save the collection', function(done){
+      var col = client.collection({ title: 'Ferrets' });
+      col.save(done);
     })
   })
 })
