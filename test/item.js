64e68f5dce35354f1267ee04dc2f6629990f1017
TJ Holowaychuk <tj@vision-media.ca>
2013-02-08 00:18:51 +0000

    remove large test fixtures, double test speed

--- test/item.js
+++ test/item.js
@@ -12,9 +12,9 @@ describe('Item', function(){
   describe('.save(fn)', function(){
     describe('when a file is given', function(){
       it('should create the item and upload the file', function(done){
-        var col = client.collection({ title: 'Maru photos' });
-        var item = col.item({ title: 'Maru 1' });
-        item.file('test/fixtures/maru-1.jpg');
+        var col = client.collection({ title: 'Files' });
+        var item = col.item({ title: 'package' });
+        item.file('package.json');
         col.save(function(err){
           if (err) return done(err);
           assert(item._id);
@@ -26,9 +26,9 @@ describe('Item', function(){
 
   describe('.remove(fn)', function(){
     it('should remove the item', function(done){
-      var col = client.collection({ title: 'Maru photos' });
-      var item = col.item({ title: 'Maru 1' });
-      item.file('test/fixtures/maru-1.jpg');
+      var col = client.collection({ title: 'Cloudup client' });
+      var item = col.item({ title: 'Something' });
+      item.file('lib/client.js');
       col.save(function(err){
         if (err) return done(err);
         item.remove(done);
